1ff2e49aa9d04fd4e743922b97f199e806f85ba5
Rafael Zucon <rafaelzucon@yahoo.com.br>
2023-01-18 16:38:38 -0300

    producer

---
+++ src/main/java/com/simpleexample/kafka_producer_consumer/SimpleController.java
@@ -0,0 +1,26 @@
+package com.simpleexample.kafka_producer_consumer;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/kafka")
+public class SimpleController {
+
+    private KafkaTemplate<String, SimpleModel> kafkaTemplate;
+    @Autowired
+    public SimpleController(KafkaTemplate<String, SimpleModel> kafkaTemplate){
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping
+    public void post(@RequestBody SimpleModel simpleModel){
+        kafkaTemplate.send("myTopic", simpleModel);
+    }
+
+}
